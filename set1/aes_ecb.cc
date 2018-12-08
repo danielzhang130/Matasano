f406f3d34efafd4a071615d04a04c37eb875114a
Daniel Zhang <danielzhang130@gmail.com>
2018-12-08 17:29:09 -0500

    Modulized aes ecb mode decryption.

---
+++ set1/aes_ecb.cc
@@ -0,0 +1,77 @@
+#include <openssl/evp.h>
+#include <cstdlib>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+
+using std::string;
+using std::cin;
+using std::cerr;
+using std::endl;
+using std::vector;
+using std::runtime_error;
+
+vector<int> aes_ecb_decrypt(string _key, vector<int> _hex){
+    EVP_CIPHER_CTX* ctx = EVP_CIPHER_CTX_new();
+    
+    //string _key;
+    //getline(cin, _key);
+    unsigned char* key = (unsigned char*) malloc(sizeof(char)*_key.size()+1);
+    if(NULL == key){
+        throw runtime_error("ERROR: malloc failed on key.");
+    }
+    for(size_t i = 0; i < _key.size(); i++){
+        key[i] = _key[i];
+    }
+    key[_key.size()] = '\0';
+    
+    //string _in;
+    //for(string s; getline(cin, s);){
+    //    _in.append(s);
+    //}
+    //vector<int> _hex = b64toHex(_in, 8);
+    int inl = sizeof(char)*_hex.size();
+    unsigned char* in = (unsigned char*) malloc(inl+1);
+    if(NULL == in){
+        throw runtime_error("ERROR: malloc failed on in.");
+    }
+    for(size_t i = 0; i < _hex.size(); i++){
+        in[i] = _hex[i];
+    }
+    in[_hex.size()] = '\0';
+
+    int outl = inl + EVP_CIPHER_block_size(EVP_aes_128_ecb());
+    unsigned char* out = (unsigned char*) malloc(outl);
+    unsigned char* current = out;
+
+    if(!EVP_DecryptInit(ctx, EVP_aes_128_ecb(), key, NULL)){
+        throw runtime_error("ERROR: EVP_DecryptInit failed.");
+    }
+
+    if(!EVP_DecryptUpdate(ctx, out, &outl, in, inl)){
+        throw runtime_error("ERROR: EVP_DecryptUpdate failed.");
+    }
+
+    current += outl;
+    if(!EVP_DecryptFinal(ctx, current, &outl)){
+        throw runtime_error("ERROR: EVP_DecryptFinal failed.");
+    }
+
+    current += outl;
+    out[current-out] = '\0';
+    
+    vector<int> ret;
+    for(int i = 0; out[i] != '\0'; i++){
+        ret.push_back(out[i]);
+    }
+
+    EVP_CIPHER_CTX_free(ctx);
+    free(key);
+    free(in);
+    free(out);
+
+    //printf("%s\n", out);
+
+    return ret;
+}
