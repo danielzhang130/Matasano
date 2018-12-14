d7f7b9b0599aa512061177726e4b39d6fa88e87f
Daniel Zhang <danielzhang130@gmail.com>
2018-12-14 17:22:34 -0500

    Done padding and unpadding.

--- set2/Padding.cc
+++ set2/Padding.cc
@@ -4,7 +4,7 @@
 using std::vector;
 using std::runtime_error;
 
-vector<int> pad(vector<int>& s, unsigned int blockSize){
+void pad(vector<int>& s, unsigned int blockSize){
     if(blockSize == 0){
         throw runtime_error("Block size cannot be 0");
     }
@@ -12,5 +12,21 @@ vector<int> pad(vector<int>& s, unsigned int blockSize){
     while(s.size() % blockSize != 0){
         s.push_back(c);
     }
-    return s;
+}
+
+void unpad(vector<int>& s){
+    int sz = s.size();
+    int c = s[sz-1];
+    if(c >= sz){
+        return;
+    }
+    for(int i = 1; i < c; i++){
+        if(s[sz-1-i] != c){
+            return;
+        }
+    }
+    while(c){
+        s.pop_back();
+        c--;
+    }
 }
