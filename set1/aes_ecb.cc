73d4f5eae8746edfc47156ae03a3e137e23c26f1
Daniel Zhang <danielzhang130@gmail.com>
2018-12-14 19:34:39 -0500

    AES CBC mode done.

--- set1/aes_ecb.cc
+++ set1/aes_ecb.cc
@@ -1,9 +1,12 @@
 #include <openssl/evp.h>
+#include "../set2/Padding.cc"
 #include <cstdlib>
 #include <string>
 #include <iostream>
 #include <vector>
 #include <stdexcept>
+#include <cmath>
+#include <cstring>
 
 using std::string;
 using std::cin;
@@ -14,27 +17,26 @@ using std::runtime_error;
 
 vector<int> aes_ecb_decrypt(string _key, vector<int> _hex){
     EVP_CIPHER_CTX* ctx = EVP_CIPHER_CTX_new();
-    
-    unsigned char* key = (unsigned char*) malloc(sizeof(char)*_key.size()+1);
+
+    unsigned char* key = (unsigned char*) malloc(sizeof(char)*_key.size());
     if(NULL == key){
         throw runtime_error("ERROR: malloc failed on key.");
     }
     for(size_t i = 0; i < _key.size(); i++){
         key[i] = _key[i];
     }
-    key[_key.size()] = '\0';
     
     int inl = sizeof(char)*_hex.size();
-    unsigned char* in = (unsigned char*) malloc(inl+1);
+    unsigned char* in = (unsigned char*) malloc(inl);
     if(NULL == in){
         throw runtime_error("ERROR: malloc failed on in.");
     }
     for(size_t i = 0; i < _hex.size(); i++){
         in[i] = _hex[i];
     }
-    in[_hex.size()] = '\0';
 
-    int outl = inl + EVP_CIPHER_block_size(EVP_aes_128_ecb());
+    //int outl = inl + EVP_CIPHER_block_size(EVP_aes_128_ecb());
+    int outl = inl;
     unsigned char* out = (unsigned char*) malloc(outl);
     unsigned char* current = out;
 
@@ -42,6 +44,8 @@ vector<int> aes_ecb_decrypt(string _key, vector<int> _hex){
         throw runtime_error("ERROR: EVP_DecryptInit failed.");
     }
 
+    EVP_CIPHER_CTX_set_padding(ctx, 0);
+    
     if(!EVP_DecryptUpdate(ctx, out, &outl, in, inl)){
         throw runtime_error("ERROR: EVP_DecryptUpdate failed.");
     }
@@ -55,7 +59,7 @@ vector<int> aes_ecb_decrypt(string _key, vector<int> _hex){
     out[current-out] = '\0';
     
     vector<int> ret;
-    for(int i = 0; out[i] != '\0'; i++){
+    for(int i = 0; i < current-out; i++){
         ret.push_back(out[i]);
     }
 
@@ -64,32 +68,33 @@ vector<int> aes_ecb_decrypt(string _key, vector<int> _hex){
     free(in);
     free(out);
 
+    unpad(ret);
     return ret;
 }
 
 vector<int> aes_ecb_encrypt(string _key, vector<int> _hex){
     EVP_CIPHER_CTX* ctx = EVP_CIPHER_CTX_new();
+    int block = EVP_CIPHER_block_size(EVP_aes_128_ecb());
     
-    unsigned char* key = (unsigned char*) malloc(sizeof(char)*_key.size()+1);
+    unsigned char* key = (unsigned char*) malloc(sizeof(char)*_key.size());
     if(NULL == key){
         throw runtime_error("ERROR: malloc failed on key.");
     }
     for(size_t i = 0; i < _key.size(); i++){
         key[i] = _key[i];
     }
-    key[_key.size()] = '\0';
     
+    pad(_hex, block);
     int inl = sizeof(char)*_hex.size();
-    unsigned char* in = (unsigned char*) malloc(inl+1);
+    unsigned char* in = (unsigned char*) malloc(inl);
     if(NULL == in){
         throw runtime_error("ERROR: malloc failed on in.");
     }
     for(size_t i = 0; i < _hex.size(); i++){
         in[i] = _hex[i];
     }
-    in[_hex.size()] = '\0';
 
-    int outl = inl + EVP_CIPHER_block_size(EVP_aes_128_ecb());
+    int outl = inl + block;
     unsigned char* out = (unsigned char*) malloc(outl);
     unsigned char* current = out;
 
@@ -106,15 +111,14 @@ vector<int> aes_ecb_encrypt(string _key, vector<int> _hex){
         throw runtime_error("ERROR: EVP_EnryptFinal failed.");
     }
 
-    current += outl;
-    out[current-out] = '\0';
+    EVP_CIPHER_CTX_free(ctx);
     
     vector<int> ret;
-    for(int i = 0; out[i] != '\0'; i++){
+    int sz = ceil(inl/block)*block;
+    for(int i = 0; i < sz; i++){
         ret.push_back(out[i]);
     }
 
-    EVP_CIPHER_CTX_free(ctx);
     free(key);
     free(in);
     free(out);
---
+++ set2/aes_cbc.cc
@@ -0,0 +1,71 @@
+#include "../set1/aes_ecb.cc"
+#include "../set1/FixedXOR.cc"
+#include <vector>
+#include <stdexcept>
+
+using std::runtime_error;
+using std::vector;
+
+vector<int> aes_cbc_encrypt(string _key, vector<int> _char, vector<int> _iv){
+    vector<int> out;
+    vector<vector<int>> blocks;
+    for(size_t i = 0; i < _char.size(); i+=16){
+        vector<int>::iterator end = (_char.end() > _char.begin() +i +16) ? _char.begin()+i+16 : _char.end();
+        vector<int> t(_char.begin()+i, end);
+        pad(t, 16);
+        blocks.push_back(t);
+    }
+    
+    if(16 != _iv.size()){
+        throw runtime_error("Initialization vector has incorrect size.");
+    }
+
+    for(size_t i = 0; i < blocks.size(); i++){
+        if(0 == i){
+            blocks[i] = fixedXOR(_iv, blocks[i]);
+        }
+        else{
+            blocks[i] = fixedXOR(blocks[i-1], blocks[i]);
+        }
+        blocks[i] = aes_ecb_encrypt(_key, blocks[i]);
+    }
+
+    for(size_t i = 0; i < blocks.size(); i++){
+        out.insert(out.end(), blocks[i].begin(), blocks[i].end());
+    }
+    return out;
+}
+
+vector<int> aes_cbc_decrypt(string _key, vector<int> _char, vector<int> _iv){
+    vector<int> out;
+    vector<vector<int>> blocks;
+    for(size_t i = 0; i < _char.size(); i+=16){
+        vector<int> t(_char.begin()+i, _char.begin()+i+16);
+        blocks.push_back(t);
+    }
+
+    if(16 != _iv.size()){
+        throw runtime_error("Initialization vector has incorrect size.");
+    }
+
+    vector<int> prev;
+    for(size_t i = 0; i < blocks.size(); i++){
+        vector<int> temp = blocks[i];
+        blocks[i] = aes_ecb_decrypt(_key, blocks[i]);
+        pad(blocks[i], 16);
+
+        if(0 == i){
+            blocks[i] = fixedXOR(_iv, blocks[i]);
+        }
+        else{
+            blocks[i] = fixedXOR(prev, blocks[i]);
+        }
+        prev = temp;
+    }
+    
+    for(size_t i = 0; i < blocks.size(); i++){
+        out.insert(out.end(), blocks[i].begin(), blocks[i].end());
+    }
+    unpad(out);
+    return out;
+}
