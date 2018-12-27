5678c206e71a8669615f57576de1338dcb4a7731
Daniel Zhang <danielzhang130@gmail.com>
2018-12-27 13:40:41 -0500

    Modulized random bit generation.

---
+++ set2/random.cc
@@ -0,0 +1,20 @@
+#include <openssl/rand.h>
+#include <vector>
+#include <stdexcept>
+#include <string>
+
+using std::vector;
+using std::runtime_error;
+using std::to_string;
+
+vector<int> random(int size){
+    unsigned char* c = (unsigned char*) malloc(size);
+    if(RAND_bytes(c, size) != 1){
+        throw runtime_error("ERROR: unable to generate random number of size " + to_string(size));
+    }
+    vector<int> v;
+    for(int i = 0; i < size; i++){
+        v.push_back(c[i]);
+    }
+    return v;
+}
